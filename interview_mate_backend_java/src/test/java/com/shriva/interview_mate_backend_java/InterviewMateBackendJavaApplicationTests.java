59761dd58a55e72cf9fd7156206fcde161058b23
Aryan Shriva <aryanshriva08@gmail.com>
2025-06-04 07:39:25 +0530

    Initial commit: Added frontend and backend projects

---
+++ interview_mate_backend_java/src/test/java/com/shriva/interview_mate_backend_java/InterviewMateBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shriva.interview_mate_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewMateBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
