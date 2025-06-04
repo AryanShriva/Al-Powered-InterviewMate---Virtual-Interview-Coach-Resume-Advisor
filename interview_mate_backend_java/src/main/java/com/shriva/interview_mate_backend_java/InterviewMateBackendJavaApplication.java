5e8404b5f018831467b6a6737e1f44ec5d9a35ed
Aryan Shriva <89904554+AryanShriva@users.noreply.github.com>
2025-06-04 07:45:40 +0530

    Backend initial structure

---
+++ interview_mate_backend_java/src/main/java/com/shriva/interview_mate_backend_java/InterviewMateBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.shriva.interview_mate_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewMateBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewMateBackendJavaApplication.class, args);
+	}
+
+}
